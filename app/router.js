46c191dffe60fae841daeedaf3d83c5f094136c9
Joseph Baik <jbaik@adcinternal.com>
2016-06-20 13:50:00 -0400

    lesson 4 minus hw

--- app/router.js
+++ app/router.js
@@ -2,19 +2,22 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.route('about');
-  this.route('contacts');
 
-  this.route('admin', function() {
-    this.route('invitations');
-  });
+    this.route('about');
+    this.route('contacts');
+
+    this.route('admin', function() {
+        this.route('invitations');
+        // this.route('contacts');
+    });
 
     this.route('libraries', function() {
         this.route('new');
+        this.route('edit', { path: '/:library_id/edit' });
     });
 });
 
--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -1,12 +1,20 @@
-/**
- * Created by jbaik on 6/20/2016.
- */
 import Ember from 'ember';
 
 export default Ember.Route.extend({
 
     model() {
         return this.store.findAll('library');
+    },
+
+    actions: {
+
+        deleteLibrary(library) {
+            let confirmation = confirm('Are you sure?');
+
+            if (confirmation) {
+                library.destroyRecord();
+            }
+        }
     }
 
-});
+})
